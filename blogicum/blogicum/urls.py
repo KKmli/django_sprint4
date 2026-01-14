5b8c4f53b3f48a58b79ffd30048fde6bf210e4a8
Alina Aspednikova <lina.aspednikova@gmail.com>
2026-01-15 00:42:16 +0300

    Исправление test_custom_err_handlers

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -2,15 +2,6 @@ from django.contrib import admin
 from django.urls import include, path, reverse_lazy
 from django.views.generic.edit import CreateView
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.views import (
-    LoginView,
-    PasswordChangeView,
-    PasswordChangeDoneView,
-    PasswordResetView,
-    PasswordResetDoneView,
-    PasswordResetConfirmView,
-    PasswordResetCompleteView,
-)
 from django.conf import settings
 from blog.views import simple_logout
 
@@ -24,15 +15,9 @@ urlpatterns = [
     path('pages/', include('pages.urls')),
     path('admin/', admin.site.urls),
 
-    path('auth/login/', LoginView.as_view(), name='login'),
+    # logout и регистрацию ПЕРЕД подключением django.contrib.auth.urls
     # Используем кастомный logout, который принимает GET-запрос
     path('auth/logout/', simple_logout, name='logout'),
-    path('auth/password_change/', PasswordChangeView.as_view(), name='password_change'),
-    path('auth/password_change/done/', PasswordChangeDoneView.as_view(), name='password_change_done'),
-    path('auth/password_reset/', PasswordResetView.as_view(), name='password_reset'),
-    path('auth/password_reset/done/', PasswordResetDoneView.as_view(), name='password_reset_done'),
-    path('auth/reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('auth/reset/done/', PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 
     path(
         'auth/registration/',
@@ -43,6 +28,8 @@ urlpatterns = [
         ),
         name='registration',
     ),
+    # маршруты для работы с пользователями из django.contrib.auth.urls
+    path('auth/', include('django.contrib.auth.urls')),
 ]
 
 if settings.DEBUG:
